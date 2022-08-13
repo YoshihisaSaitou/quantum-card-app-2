17241a27e010aad2cf98e22649a28fe2ed4a1028
Yoshihisa Saito <anti_viral_drug@yahoo.co.jp>
2022-08-14 08:07:37 +0900

    first commit

---
+++ nuxt-app/nuxt.config.ts
@@ -0,0 +1,13 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    ssr: false,
+    target: "static",
+    //vite: {
+    //    clearScreen: false
+    //},
+    typescript: {
+        strict: true
+    }
+})
